81ac606d6de938b43268f41e83181bded427543c
tenkoh <tenkoh.go@gmail.com>
2024-12-15 07:56:14 +0900

    リファクタリング

--- main.go
+++ main.go
@@ -29,33 +29,39 @@ type User struct {
 	ID int `json:"id"`
 }
 
+type Repository struct {
+	client *s3.Client
+}
+
+func NewRepository(client *s3.Client) *Repository {
+	return &Repository{client}
+}
+
+func (r *Repository) GetUsers(ctx context.Context, bucket, key string) []User {
+	object, _ := r.client.GetObject(ctx, &s3.GetObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+	})
+	defer object.Body.Close()
+
+	var users []User
+	json.NewDecoder(object.Body).Decode(&users)
+	return users
+}
+
 func main() {
+	// 説明のためにエラーハンドリングは割愛
 	ctx := context.Background()
 	s3Config := newS3Config()
 
-	config, err := config.LoadDefaultConfig(ctx, config.WithRegion(s3Config.region))
-	if err != nil {
-		panic(err)
-	}
+	config, _ := config.LoadDefaultConfig(ctx, config.WithRegion(s3Config.region))
 
 	client := s3.NewFromConfig(config, func(o *s3.Options) {
 		o.UsePathStyle = true
 	})
 
-	object, err := client.GetObject(ctx, &s3.GetObjectInput{
-		Bucket: aws.String(s3Config.bucket),
-		Key:    aws.String(s3Config.key),
-	})
-
-	if err != nil {
-		panic(err)
-	}
-	defer object.Body.Close()
-
-	var users []User
-	if err := json.NewDecoder(object.Body).Decode(&users); err != nil {
-		panic(err)
-	}
+	repository := NewRepository(client)
+	users := repository.GetUsers(ctx, s3Config.bucket, s3Config.key)
 
 	fmt.Printf("%+v", users)
 }
